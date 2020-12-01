40861030b13430d2169009cb6a92f3f08c721ecb
Eyal-Hashim <59773129+Eyal-Hashim@users.noreply.github.com>
2020-12-01 16:44:20 +0200

    Update city_state.py

--- city_state.py
+++ city_state.py
@@ -1,3 +1,20 @@
+states_abb = ["AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL",
+              "IN", "KS",
+              "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV",
+              "NY", "OH",
+              "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"]
+states_long = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado",
+               "Connecticut",
+               "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho",
+               "Illinois",
+               "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan",
+               "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska",
+               "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon",
+               "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee",
+               "Texas",
+               "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia",
+               "Wyoming"]
+
 city_to_state_dict = {"East Rancho Dominguez": "California",
                       "Clinton": "Mississippi",
                       "Nanuet": "New York",
